84d3f93f16d2d16e4372ab6046b1e901882681a3
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2022-03-08 23:09:04 +0300

    дженерик для стэка

--- src/test/java/com/tape/town/Stack.java
+++ src/test/java/com/tape/town/Stack.java
@@ -3,26 +3,27 @@ package com.tape.town;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
-public class Stack implements Cloneable {
-    private Object[] elements;
+public class Stack<T> implements Cloneable {
+    private T[] elements;
     private int size = 0;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
+    @SuppressWarnings("unchecked")
     public Stack() {
-        this.elements = new Object[DEFAULT_INITIAL_CAPACITY];
+        this.elements = (T[]) new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
-    public void push(Object e) {
+    public void push(T e) {
         ensureCapacity();
         elements[size++] = e;
     }
 
-    public Object pop() {
+    public T pop() {
         if (size == 0) {
             throw new EmptyStackException();
         }
 
-        Object result = elements[--size];
+        T result = elements[--size];
         elements[size] = null;
 
         return result;
@@ -35,8 +36,8 @@ public class Stack implements Cloneable {
     }
 
     @Override
-    public Stack clone() throws CloneNotSupportedException {
-        Stack result = (Stack) super.clone();
+    public Stack<T> clone() throws CloneNotSupportedException {
+        @SuppressWarnings("unchecked") Stack<T> result = (Stack<T>) super.clone();
         result.elements = elements.clone();
 
         return result;
--- src/test/java/com/tape/town/StackTest.java
+++ src/test/java/com/tape/town/StackTest.java
@@ -2,17 +2,14 @@ package com.tape.town;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StackTest {
-    private final Stack stack = new Stack();
+    private final Stack<PhoneNumber> stack = new Stack<>();
 
     @Test
     public void push() throws CloneNotSupportedException {
         stack.push(new PhoneNumber(111, 222, 4444));
 
-        Stack cloned = stack.clone();
-
+        var cloned = stack.clone();
 
         System.out.println(stack.pop());
         System.out.println(cloned.pop());
